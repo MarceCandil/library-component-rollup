e78de30498031be4e3292d8ff1e6e51dfd13ca01
Marcelo Candil <marcecandil@gmail.com>
2022-06-13 15:48:50 -0300

    changed structure

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import './button.css';
+import './Button.scss';
 
 export interface ButtonProps {
   /**
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,1 @@
-export type { ButtonProps } from './button';
-export { default as Button } from './button';
+export { default as Button } from "./Button";
